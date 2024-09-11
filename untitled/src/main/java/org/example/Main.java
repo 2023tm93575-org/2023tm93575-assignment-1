35fc2610b2e9fc2d0adb50fe52f1648643ae802f
ashwiniwaghchaure <154862692+ashwiniwaghchaure@users.noreply.github.com>
2024-09-11 23:29:09 +0530

    code change

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("ABCD");
+        System.out.println("PQRS");
+//        random comment
+        System.out.println("ABCD");
+
     }
 }
